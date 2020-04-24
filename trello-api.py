cb5bbdf21b5c29acdba4f4c5acc59da56c32ef3b
minaton-ru <53541518+minaton-ru@users.noreply.github.com>
2020-04-24 23:53:39 +0300

    Доработана функция move

--- trello-api.py
+++ trello-api.py
@@ -6,23 +6,22 @@ auth_params = {
     'key': "",    
     'token': "", }  
   
-# Адрес, на котором расположен API Trello, # Именно туда мы будем отправлять HTTP запросы.  
+# Адрес, на котором расположен API Trello 
 base_url = "https://api.trello.com/1/{}"
-board_id = "M84PWQ9O"
+board_id = ""
 
 def read():      
-    # Получим данные всех колонок на доске:      
+    # Получим данные всех колонок на доске  
     column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
-    # Теперь выведем название каждой колонки и всех заданий, которые к ней относятся:      
-    for column in column_data:
-        # Получим данные всех задач в колонке и перечислим все названия      
+    # Выведем название каждой колонки и всех заданий, которые к ней относятся:      
+    for column in column_data:     
         task_data = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()      
         print('\n=====' + column['name'] + '===== (количество задач: ' + str(len(task_data)) + ')')
         if not task_data:      
             print('\t' + 'Нет задач!')      
             continue      
         for task in task_data:      
-            print('\t - ' + task['name'] + ' [id: ' + str(task['idShort']) + ']')  
+            print('\t - ' + task['name'] + ' [idShort: ' + str(task['idShort']) + ']')  
 
 def create(name, column_name):      
     # Получим данные всех колонок на доске      
@@ -30,7 +29,7 @@ def create(name, column_name):
     # Переберём данные обо всех колонках, пока не найдём ту колонку, которая нам нужна      
     for column in column_data:      
         if column['name'] == column_name:      
-            # Создадим задачу с именем _name_ в найденной колонке      
+            # Создадим задачу с именем name в найденной колонке      
             requests.post(base_url.format('cards'), data={'name': name, 'idList': column['id'], **auth_params})
             break
 
@@ -40,20 +39,20 @@ def createColumn(name):
     # Создаем новую колонку (список) в нашей доске
     requests.post(base_url.format('lists'), data={'name': name, 'idBoard': responseget['id'], **auth_params})
 
-def move(name, column_name):    
+def move(taskShortID, column_name):    
     # Получим данные всех колонок на доске    
     column_data = requests.get(base_url.format('boards') + '/' + board_id + '/lists', params=auth_params).json()
-    # Среди всех колонок нужно найти задачу по имени и получить её id    
+    # Среди всех колонок нужно найти задачу по короткому id и получить её длинный id    
     task_id = None    
     for column in column_data:    
-        column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()    
-        for task in column_tasks:    
-            if task['name'] == name:    
-                task_id = task['id']    
+        column_tasks = requests.get(base_url.format('lists') + '/' + column['id'] + '/cards', params=auth_params).json()   
+        for task in column_tasks:
+            if taskShortID == str(task['idShort']):    
+                task_id = task['id']     
                 break    
         if task_id:    
             break    
-    # Теперь, когда у нас есть id задачи, которую мы хотим переместить    
+    # Теперь, когда у нас есть длинный id задачи, которую мы хотим переместить    
     # переберём данные обо всех колонках, пока не найдём ту, в которую мы будем перемещать задачу    
     for column in column_data:    
         if column['name'] == column_name:    
